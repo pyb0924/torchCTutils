cf2723d5b8469aebd9ddd5c8beb6dd0b97ecc138
pyb0924 <pyb0924@sjtu.edu.cn>
2023-10-16 17:43:58 +0800

    10.16

--- tests/models/test_resnet.py
+++ tests/models/test_resnet.py
@@ -11,6 +11,13 @@ def run_resnet_model_2d(model, size):
     assert output.shape == torch.Size([8, 2])
 
 
+def run_resnet_model_3d(model, size):
+    assert model is not None
+    data = torch.rand((8, 1, size, size, size))
+    output = model(data)
+    assert output.shape == torch.Size([8, 2])
+
+
 def test_resnet_2d(size):
     resnet34 = resnet34_2d()
     run_resnet_model_2d(resnet34, size)
@@ -22,3 +29,14 @@ def test_resnet_2d(size):
     resnet50_cbam = resnet50_2d(use_attention="CBAM")
     run_resnet_model_2d(resnet50_cbam, size)
 
+
+def test_resnet_3d(size):
+    resnet34 = resnet34_3d()
+    run_resnet_model_3d(resnet34, size)
+    resnet34_cbam = resnet34_3d(use_attention="CBAM")
+    run_resnet_model_3d(resnet34_cbam, size)
+
+    resnet50 = resnet50_3d()
+    run_resnet_model_3d(resnet50, size)
+    resnet50_cbam = resnet50_3d(use_attention="CBAM")
+    run_resnet_model_3d(resnet50_cbam, size)
--- torchCTutils/io/preprocess.py
+++ torchCTutils/io/preprocess.py
@@ -97,10 +97,10 @@ def window_normalize(
 
 
 def get_bbox_from_mask(mask: np.array):
-    nonzero_indexs=np.nonzero(mask)
-    result=[]
+    nonzero_indexs = np.nonzero(mask)
+    result = []
     for index in nonzero_indexs:
-        result.append([np.min(index),np.max(index)])
+        result.append([np.min(index), np.max(index)])
     return np.array(result)
 
 
